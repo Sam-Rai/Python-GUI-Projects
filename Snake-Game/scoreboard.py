7a5fc7011e2f552ae72b7c127c85d398c72aa266
Sam Rai <samrajalimrai@gmail.com>
2024-12-10 22:57:48 -0500

    modified the Snake-Game with high score recorded from the past games

--- Snake-Game/scoreboard.py
+++ Snake-Game/scoreboard.py
@@ -10,21 +10,31 @@ class Score(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
+        with open("data.txt") as file:
+            text = file.read()
+        self.high_score = int(text)
         self.penup()
         self.goto(0, 260)
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
-
-
